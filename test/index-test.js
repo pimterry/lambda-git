edbbb2e28e4e0cb2f842242c0acefe93288cf25b
Ryan Block <rblock@gmail.com>
2019-11-09 20:04:34 -0800

    Add tape-base tests, update deps

---
+++ test/index-test.js
@@ -0,0 +1,102 @@
+let test = require('tape')
+let exec = require('child_process').exec
+let fs = require('fs')
+let exists = fs.existsSync
+let lambdaGit = require('../')
+
+function mkTmp() {
+  return fs.mkdtempSync('/tmp/lambda-git-')
+}
+
+function reset (callback) {
+  exec('rm -rf /tmp/git /tmp/lambda-git-*', err => {
+    if (err) throw Error(err)
+    let tmp = fs.readdirSync('/tmp')
+    let clean = tmp.every(f => f !== 'git' && !f.startsWith('lambda-git'))
+    if (!clean) throw Error('/tmp is not clean')
+    callback()
+  })
+}
+
+test('Env check', t => {
+  t.plan(1)
+  t.ok(lambdaGit, 'Module present')
+})
+
+test('Lib returns a Promise', t => {
+  reset(async () => {
+    t.plan(1)
+    let isPromise = lambdaGit() instanceof Promise
+    setTimeout(() => {
+      // "await" while file ops finish before proceeding
+      t.ok(isPromise, 'Promise returned')
+    }, 500)
+  })
+})
+
+test('Install the git binary to /tmp/git by default (async)', t => {
+  reset(async () => {
+    t.plan(1)
+    await lambdaGit()
+    t.ok(exists('/tmp/git/usr/bin/git'), 'Git binary exists on the filesystem')
+  })
+})
+// TODO continuation passing version
+
+test('Install the git binary to a given dir and set env vars (async)', t => {
+  reset(async () => {
+    t.plan(4)
+    let targetDirectory = mkTmp()
+    console.log(`Installing to target: ${targetDirectory}`)
+    await lambdaGit({targetDirectory})
+
+    t.ok(exists(`${targetDirectory}/usr/bin/git`), 'Git binary exists on the filesystem')
+
+    let pathEnvAdded = process.env.PATH.includes(`${targetDirectory}/usr/bin`)
+    t.ok(pathEnvAdded, `Env $PATH includes git path: ${process.env.PATH}`)
+
+    // Ok instead of equal because it's getting a bit unreadable
+    let templateEnvAdded = process.env.GIT_TEMPLATE_DIR === `${targetDirectory}/usr/share/git-core/templates`
+    t.ok(templateEnvAdded, `Env includes git template dir: ${process.env.GIT_TEMPLATE_DIR}`)
+
+    let execEnvAdded = process.env.GIT_EXEC_PATH === `${targetDirectory}/usr/libexec/git-core`
+    t.ok(execEnvAdded, `Env includes git exec path: ${process.env.GIT_EXEC_PATH}`)
+  })
+})
+// TODO continuation passing version
+
+test('Disable env mutation (async)', t => {
+  reset(async () => {
+    t.plan(3)
+    let targetDirectory = mkTmp()
+    console.log(`Installing to target: ${targetDirectory}`)
+    await lambdaGit({targetDirectory, updateEnv: false})
+
+    let pathEnvAdded = process.env.PATH.includes(`${targetDirectory}/usr/bin`)
+    t.notOk(pathEnvAdded, `Env $PATH does not include git path: ${process.env.PATH}`)
+
+    let templateEnvAdded = process.env.GIT_TEMPLATE_DIR === `${targetDirectory}/usr/share/git-core/templates`
+    t.notOk(templateEnvAdded, `Env does not include git template dir: ${process.env.GIT_TEMPLATE_DIR}`)
+
+    let execEnvAdded = process.env.GIT_EXEC_PATH === `${targetDirectory}/usr/libexec/git-core`
+    t.notOk(execEnvAdded, `Env does not include git exec path: ${process.env.GIT_EXEC_PATH}`)
+  })
+})
+
+test('Promise returns env vars when env mutation is disabled (async)', t => {
+  reset(async () => {
+    t.plan(4)
+    let targetDirectory = mkTmp()
+    console.log(`Installing to target: ${targetDirectory}`)
+    let git = await lambdaGit({targetDirectory, updateEnv: false})
+
+    t.equal(git.binPath, `${targetDirectory}/usr/bin`, `Returned binPath: ${git.binPath}`)
+    t.equal(git.env.GIT_TEMPLATE_DIR, `${targetDirectory}/usr/share/git-core/templates`, `Returned env.GIT_TEMPLATE_DIR: ${git.env.GIT_TEMPLATE_DIR}`)
+    t.equal(git.env.GIT_EXEC_PATH, `${targetDirectory}/usr/libexec/git-core`, `Returned env.GIT_EXEC_PATH: ${git.env.GIT_EXEC_PATH}`)
+    t.equal(git.env.LD_LIBRARY_PATH, `${targetDirectory}/usr/lib64`, `Returned env.LD_LIBRARY_PATH: ${git.env.LD_LIBRARY_PATH}`)
+  })
+})
+
+test('Clean up', t => {
+  reset(t.end)
+})
